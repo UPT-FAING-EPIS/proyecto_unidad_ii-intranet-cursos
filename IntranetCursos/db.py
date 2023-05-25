c372ac9a3518623fbb5ab9e8af99e5b49d30f013
Erick Mauricio <102829461+Erick03072003@users.noreply.github.com>
2023-05-25 17:33:21 -0500

    Delete IntranetCursos directory

--- IntranetCursos/db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-# Configuración de la conexión a MongoDB
-client = MongoClient('mongodb://localhost:27017/')
-db = client['Flaskql']
-collectionGrades = db['grades']
