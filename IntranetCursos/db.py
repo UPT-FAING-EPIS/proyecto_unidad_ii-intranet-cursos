da8409d03a300944663c7b7a915d73bdb9ee2c45
HelbertCLinUPT <hc2020067571@virtual.upt.pe>
2023-05-27 14:39:51 -0500

    UpdateRabbitmq

--- IntranetCursos/db.py
+++ IntranetCursos/db.py
@@ -3,4 +3,5 @@ from pymongo import MongoClient
 # Configuración de la conexión a MongoDB
 client = MongoClient('mongodb+srv://admin:admin@mimongodbhelbert.iohwb3r.mongodb.net/')
 db = client['Flaskql']
+
 collectionGrades = db['grades']
--- IntranetCursos/queries.py
+++ IntranetCursos/queries.py
@@ -2,12 +2,14 @@ from typesgraphs import Grade
 import graphene
 from db import collectionGrades
 from bson import ObjectId
-
+from rabitmqconnect import rabbitmqMessage
 # Definición de la consulta "Query" en GraphQL
 
 def resolve_allGrades(self, info):
     # Obtener todos los registros de la base de datos
     grades = collectionGrades.find()
+    grades_message=list(grades)
+    rabbitmqMessage(f"Total grades found: {len(grades_message)}", "all_grades")
     return [Grade(
         id=str(grade['_id']),  # Agregar el campo 'id' con el valor del _id de MongoDB
         codigoMatricula=grade['codigoMatricula'],
@@ -15,7 +17,7 @@ def resolve_allGrades(self, info):
         nombresAlumno=grade['nombresAlumno'],
         apellidosAlumno=grade['apellidosAlumno'],
         curso=grade['curso']
-    ) for grade in grades]
+    ) for grade in grades_message]
 
 def resolve_gradeById(self, info, id):
     if id:
@@ -25,6 +27,8 @@ def resolve_gradeById(self, info, id):
         # Si no se proporciona el ID, retornar None
         return None
 
+    rabbitmqMessage(f"Resolve_GradeById: {id}", "grade_by_id")
+    
     if grade:
         return Grade(
             codigoMatricula=grade['codigoMatricula'],
@@ -48,6 +52,8 @@ def resolve_gradeByCodigoMatricula(self, info, codigoMatricula):
             curso=grade['curso']
         ) for grade in grades]
         print(grade_list)
+        rabbitmqMessage(f"Resolve_GradeByCodigoMatricula: {codigoMatricula}", "grade_by_codigo_matricula")
+
         return grade_list
     else:
         # Si no se proporciona el código de matrícula, retornar None
