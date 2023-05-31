32e94ec1a3dc94774181d1ec19111a2d2255a596
HelbertinUPT <hc2020067571@virtual.upt.pe>
2023-05-30 20:30:01 -0500

    UPDATE_Rabbit

--- IntranetCursos/rabitmqconnect.py
+++ IntranetCursos/rabitmqconnect.py
@@ -1,22 +1,29 @@
 import pika
+import datetime 
+import json
+
 rabbitmq_host = "localhost"
 rabbitmq_port = 5672
-rabbitmq_exchange = "grades"
 
 
 def rabbitmqMessage(message="error", routing_key="error"):
+
+    log={
+        "timestamp": datetime.datetime.now().isoformat(),
+        "Level": routing_key,
+        "Message": json.dumps(message)
+    }
+
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port)
     )
-    
-
-    
+        
     channel = connection.channel()
 
-    channel.queue_declare(queue=routing_key)
+    channel.queue_declare(queue="logs")
 
     channel.basic_publish(
-        exchange='', routing_key=routing_key, body=message
+        exchange='', routing_key="logs", body=json.dumps(log)
     )
 
     connection.close()
